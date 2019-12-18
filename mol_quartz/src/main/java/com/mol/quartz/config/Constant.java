0bea7af44376206dd965ae343e1c233b37d86524
earnop <liuyangnop@163.com>
2019-12-18 14:40:06 +0800

    修改日志文件路径，为/usr/local/log/项目名称/项目名称.log;管理后端注册为eureka客户端

--- mol_quartz/src/main/java/com/mol/quartz/config/Constant.java
+++ mol_quartz/src/main/java/com/mol/quartz/config/Constant.java
@@ -1,54 +1,32 @@
 package com.mol.quartz.config;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
 @Configuration
+@Data
 public class Constant {
 
     /**
      * E应用的agentdId，登录开发者后台可查看
      */
-    @Value("${purchase.agentid}")
+    @Value("${purchaseagentid}")
     public Long purchaseAgentId ;
 //    public static final Long AGENTID = 330159066L;
 
     /**
      * 第三方报价平台agentdId
      */
-    @Value("{supplier.agentid}")
+    @Value("${supplieragentid}")
     public long supplierAgentId;
 
     /**
      *专家端agentId
      */
-    @Value("{expert.agentid}")
+    @Value("${expertagentid}")
     public long expertAgentId;
 
-
-    public Long getPurchaseAgentId() {
-        return purchaseAgentId;
-    }
-
-    public void setPurchaseAgentId(Long purchaseAgentId) {
-        this.purchaseAgentId = purchaseAgentId;
-    }
-
-    public long getSupplierAgentId() {
-        return supplierAgentId;
-    }
-
-    public void setSupplierAgentId(long supplierAgentId) {
-        this.supplierAgentId = supplierAgentId;
-    }
-
-    public long getExpertAgentId() {
-        return expertAgentId;
-    }
-
-    public void setExpertAgentId(long expertAgentId) {
-        this.expertAgentId = expertAgentId;
-    }
 }
